f6d4c22bcf8cada6dc6358eca6d696b1a5f9369c
MandosGrogu <jpinkmann@mail.ru>
2026-04-19 13:39:48 +0300

    chenged path creation

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,11 @@
+import os
 from gendiff.scripts.gendiff import generate_diff
 
 def test_gendiff():
 
-    file1_path = "test_data/f1.json"
-    file2_path = "test_data/f2.json"
+    base_path = os.getenv('GITHUB_WORKSPACE', os.getcwd())
+    file1_path = os.path.join(base_path, 'test_data/f1.json')
+    file2_path = os.path.join(base_path, 'test_data/f2.json')
 
     assert generate_diff(file1_path, file2_path) == '''- follow: False
   host: hexlet.io
